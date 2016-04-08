f0f3d70f9975040ac2dc39ad547e7436ec4548a0
davidwei <david.wei@ringierchina.com>
2016-04-08 18:08:16 +0800

    04-08

--- scripts/copy_users_from_django_to_laravel.py
+++ scripts/copy_users_from_django_to_laravel.py
@@ -2,12 +2,19 @@ import psycopg2
 import MySQLdb
 import datetime, time
 
-
+'''
 psycopg2_conn = psycopg2.connect(
         database="expat", 
         user="david", 
         password="1", 
         host="localhost", 
+        port="5432"
+    ) '''
+psycopg2_conn = psycopg2.connect(
+        database="expat", 
+        user="ringier", 
+        password="1qaz,2wsx", 
+        host="expat-posgres-0327.cps3uyndw5ab.ap-northeast-1.rds.amazonaws.com", 
         port="5432"
     ) 
 mysql_conn= MySQLdb.connect(
@@ -25,6 +32,7 @@ def get_user_data():
     cur.execute('''
     select * from auth_user join accounts_myprofile 
     on auth_user.id = accounts_myprofile.user_id
+    where auth_user.username = 'hehe'
     limit 20;
         ''')
     rows = cur.fetchall() 
@@ -65,7 +73,7 @@ def save_user_to_mysql(rows):
             insert into users(id, name, email, password, activated, banned,last_login,protected, created_at) 
                        values(%d, '%s', '%s',  '%s',     %s,        %d,    '%s',        %d,        '%s')
         ''' % tuple(userrow)
-        
+        print sql
         cur.execute(sql) 
 
         ######## insert into user group
@@ -79,42 +87,45 @@ def save_user_to_mysql(rows):
         ######## insert into profile table
         print row
         profile_row = list()
-        profile_row.append(row[11])  # profile table field  id
-        profile_row.append(row[0])   # user_id
-        profile_row.append(row[5]) if row[5] else profile_row.append('')  # first_name
-        profile_row.append(row[6]) if row[6] else profile_row.append('')  # last_name
-        profile_row.append(row[16]) if row[16] else profile_row.append('')  # phone
-        profile_row.append(row[15]) if row[15] else profile_row.append('')  # address
-        profile_row.append(created_at)   # created_at
-        profile_row.append(row[12]) if row[12] else profile_row.append('')  # mugshot
-        profile_row.append(row[13])   # privacy
-        profile_row.append(row[17]) if row[17] else profile_row.append('')  # company_name
-        profile_row.append(row[14]) if row[14] else profile_row.append('null')   # housing_uid
-        profile_row.append(row[18]) if row[18] else profile_row.append('null')   # live_area
-        profile_row.append(row[19]) if row[19] else profile_row.append('null')   # come_from
-        profile_row.append(row[20]) if row[20] else profile_row.append('null')   # come_reason
-        profile_row.append(row[21]) if row[21] else profile_row.append('null')   # gender
-        profile_row.append(row[22]) if row[22] else profile_row.append('null')   # marital
-        profile_row.append(row[23]) if row[23] else profile_row.append('null')   # kids
+        profile_row.append(row[11])  # 1 profile table field  id
+        profile_row.append(row[0])   # 2 user_id
+        profile_row.append(row[5]) if row[5] else profile_row.append('')  #3 first_name
+        profile_row.append(row[6]) if row[6] else profile_row.append('')  #4 last_name
+        profile_row.append(row[16]) if row[16] else profile_row.append('')  #5 phone
+        profile_row.append(row[15]) if row[15] else profile_row.append('')  #6 address
+        profile_row.append(created_at)   #7 created_at
+        profile_row.append(row[12]) if row[12] else profile_row.append('')  #8 mugshot
+        profile_row.append(row[13])   #9 privacy
+        profile_row.append(row[17]) if row[17] else profile_row.append('')  #10 company_name
+        profile_row.append(row[14]) if row[14] else profile_row.append('null')   #11 housing_uid
+        profile_row.append(row[18]) if row[18] else profile_row.append('null')   #12 live_area
+        profile_row.append(row[19]) if row[19] else profile_row.append('null')   #13 come_from
+        profile_row.append(row[20]) if row[20] else profile_row.append('null')   #14 come_reason
+        profile_row.append(row[21]) if row[21] else profile_row.append('null')   #15 gender
+        profile_row.append(row[22]) if row[22] else profile_row.append('null')   #16 marital
+        profile_row.append(row[23]) if row[23] else profile_row.append('null')   #17 kids
         #profile_row.append(row[26])   # is_bussiness
-        profile_row.append(1) if row[26] is True else profile_row.append(0) # is_bussiness
+        profile_row.append(1) if row[26] is True else profile_row.append(0) #18 is_bussiness
         #profile_row.append(row[27])   # is_paid_for_classifieds
-        profile_row.append(1) if row[27] is True else profile_row.append(0) # is_paid_for_classifieds
+        profile_row.append(1) if row[27] is True else profile_row.append(0) #19 is_paid_for_classifieds
         #profile_row.append(row[28])   # is_approved
-        profile_row.append(1) if row[28] is True else profile_row.append(0) # is_approved
+        profile_row.append(1) if row[28] is True else profile_row.append(0) #20 is_approved
+        print row[24],'row24'
         if row[24]:
+            print 'has birthday'
             r24 = row[24]
             birthday = datetime.datetime(r24.year, r24.month, r24.day)
             birthday = birthday.strftime('%Y-%m-%d')
             profile_row.append(birthday)
         else:
-            profile_row.append(row[24])
+            print 'no birthday'
+            profile_row.append('null')
 
 
-
+        #                         1   2        3             4         5      6        7         8         9       10             11          12         13           14         15   16        17    18            19                        20          21                                
         profile_sql = '''
         insert into user_profile(id, user_id, first_name, last_name, phone, address,created_at,mugshot, privacy,company_name,housing_uid, live_area, come_from, come_reason, gender, marital, kids, is_bussiness, is_paid_for_classifieds, is_approved, birthday) 
-                          values(%d, %d,      '%s',       '%s',      '%s',  '%s',   '%s',      '%s',    '%s',   '%s',        %s,          %s,        %s,        %s,          %s,     %s,      %s,   %s,            %d,                       %d,        '%s'  )
+                          values(%d, %d,      '%s',       '%s',      '%s',  '%s',   '%s',      '%s',    '%s',   '%s',        %s,          %s,        %s,        %s,          %s,     %s,      %s,   %s,            %d,                       %d,        %s  )
         '''% tuple(profile_row)
         print profile_sql
         cur.execute(profile_sql) 
