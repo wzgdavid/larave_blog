aa75c3a90bda255d3a3674ffa03a1524ebd22236
davidwei <david.wei@ringierchina.com>
2016-04-11 15:24:46 +0800

    can copy tables but the users table has duplicated emails

---
+++ scripts/find_duplicated_emial.py
@@ -0,0 +1,20 @@
+#encoding:utf-8
+'''
+shanghaiexpat 中有些用命令创建的superuser,所以导致了有些email不唯一
+找出这些email ，可能需要手动删掉
+不知道什么原因，重复的email有500多个 常识来说emial应该是唯一的
+
+这不是个函数，只是一个脚本，拷贝到django的shell中运行
+'''
+
+from collections import Counter
+from django.contrib.auth.models import User;
+users = User.objects.all()
+emails = []
+for user in users:
+    emails.append(user.email)
+cnt = Counter()
+for email in emails:
+    cnt[email] += 1
+
+cnt.most_common(538)
