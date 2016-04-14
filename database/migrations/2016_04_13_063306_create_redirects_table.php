de7c978e2b2e298bc02790b178a7656a5593d5de
davidwei <david.wei@ringierchina.com>
2016-04-14 14:13:46 +0800

    migrations

---
+++ database/migrations/2016_04_13_063306_create_redirects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRedirectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('redirects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('redirect_from')->unique();
+            $table->string('redirect_to')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('redirects');
+    }
+}
